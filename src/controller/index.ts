27a50f394663d9ed90969ce64c3d63a4123556c3
bryanbill <emobrya@gmail.com>
2024-03-07 12:00:51 +0300

    ft: route - implements auth route

---
+++ src/controller/index.ts
@@ -0,0 +1,7 @@
+import * as auth from './auth';
+import * as journal from './journal';
+
+export {
+    auth,
+    journal
+}
--- src/router/auth.ts
+++ src/router/auth.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-
+import { auth } from "@/controller";
 
 const router: Router = Router();
 
@@ -10,7 +10,21 @@ const router: Router = Router();
  *  - email: string
  * 
  */
-router.post('/sign-in', (req, res) => { });
+router.post('/sign-in', (req, res) => {
+    try {
+        const response = auth.signIn(req.body.email);
+        res.status(200).json({
+            status: 0,
+            message: 'OTP sent to email',
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            status: 1,
+            message: 'Internal server error',
+        });
+    }
+});
 
 /**
  * Verifies the OTP sent to the user's email
@@ -20,15 +34,24 @@ router.post('/sign-in', (req, res) => { });
  *  - otp: string
  * 
  */
-router.post('/verify-otp', (req, res) => { });
+router.post('/verify-otp', (req, res) => {
+    try {
+        const response = auth.verifyOtp(req.body.email, req.body.otp);
+        res.status(200).json({
+            status: 0,
+            message: 'OTP verified',
+            data: {
+                token: response
+            },
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            status: 1,
+            message: 'Internal server error',
+        });
+    }
+});
 
-/**
- * Refresh the user's token
- * 
- * Expects a JSON object with the following properties:
- *  - token: string
- * 
- */
-router.post('/refresh', (req, res) => { });
 
 export default router;
